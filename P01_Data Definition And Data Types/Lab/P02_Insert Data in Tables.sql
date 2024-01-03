e1c23ca29ed31747fe122289ec15556d207990ef
JulianJekov <drmsz@abv.bg>
2024-01-03 17:41:58 +0200

    Lab

--- P01_Data Definition And Data Types/Lab/P02_Insert Data in Tables.sql
+++ P01_Data Definition And Data Types/Lab/P02_Insert Data in Tables.sql
@@ -1,2 +1,2 @@
 INSERT INTO `employess` (`first_name`, `last_name`) VALUES ('Pesho', 'Peshev');
-INSERT INTO `employess` (`first_name`, `last_name`) VALUE ('Gosho', 'Goshev'), ('Toshko', 'Toshkov');
+INSERT INTO `employess` (`first_name`, `last_name`) VALUES ('Gosho', 'Goshev'), ('Toshko', 'Toshkov');
