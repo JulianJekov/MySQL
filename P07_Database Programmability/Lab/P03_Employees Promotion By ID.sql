014509f4042817eb2694c3e03bde79cf2e27d37e
JulianJekov <drmsz@abv.bg>
2024-01-16 22:09:58 +0200

    Lab
    Database Programmability Lab

---
+++ P07_Database Programmability/Lab/P03_Employees Promotion By ID.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+
+CREATE PROCEDURE usp_raise_salary_by_id (id INT)
+BEGIN
+	DECLARE count_by_id INT;
+	START TRANSACTION;
+    SET count_by_id := (SELECT COUNT(*) FROM `employees` WHERE `employee_id` = id);
+
+UPDATE `employees` AS e 
+SET 
+    `salary` = `salary` * 1.05
+WHERE
+    e.`employee_id` = id;
+    
+    IF (count_by_id < 1) THEN
+    ROLLBACK;
+    
+    ELSE
+    COMMIT;
+	END IF;
+    
+END$$
